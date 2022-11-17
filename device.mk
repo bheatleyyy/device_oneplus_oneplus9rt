54a8e1171afd122137d1ae92e903daddc07fc94b
abhinavgupta371 <abhinavgupta371@gmail.com>
2022-11-18 01:59:24 +0800

    oneplsu9rt: Import audio configs from OOS C.07

--- device.mk
+++ device.mk
@@ -13,6 +13,14 @@
 # limitations under the License.
 
 $(call inherit-product, vendor/oneplus/oneplus9rt/oneplus9rt-vendor.mk)
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/audio_platform_info_intcodec.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_mixer_paths.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_platform_info.xml
+
 $(call inherit-product, device/oneplus/oneplus9/device.mk)
 
 # Namespaces
